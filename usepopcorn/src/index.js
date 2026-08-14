bcb47b0a3d4d36c70bd6204bdf7a94d75d1dee94
Arutchelvan <arulbalaji03@gmail.com>
2026-08-14 08:58:12 +0530

    Basic Star Rating Component Created

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function RatingComp() {
+  const [getRating, setGetRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setGetRating} />
+      <p>Selected rating is {getRating} value</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxLength={5}
+      messages={["terrible", "fine", "good", "better", "best"]}
+      defaultValue={2}
+    />
+    <StarRating maxLength={5} size={24} className="test" color="red" />
+    <RatingComp />
   </React.StrictMode>,
 );
