b8f323c0a7a9a04fa7f622ce0fbfba6b2be38167
Arutchelvan <arulbalaji03@gmail.com>
2026-09-06 21:22:32 +0530

    react-quiz app started

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,50 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log(state);
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:9000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>
+          1/{questions.length} - Status : {status}
+        </p>
+        <p>Question?</p>
+        {questions.map((question) => (
+          <div key={question.id}>{question.question}</div>
+        ))}
+
+        {status === "loading" ? <Loader /> : ""}
+      </Main>
     </div>
   );
 }
