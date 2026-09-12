c9c196d0641116f8d43bbf55dd276efe2a90bcf5
Arutchelvan <arulbalaji03@gmail.com>
2026-09-12 12:02:32 +0530

    Worldwise - ReactRouter with NavLink added

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/Pricing";
+import Contact from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
 export default function App() {
-  return <h1>Hello vite</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
